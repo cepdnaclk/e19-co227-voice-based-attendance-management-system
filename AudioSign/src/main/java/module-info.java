11532303039c29522b6e2d86b874c389d478dad3
dasunsilva <115541700+dasunsilva@users.noreply.github.com>
2023-09-13 10:22:47 +0530

    Delete AudioSign directory

--- AudioSign/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.audiosign {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.audiosign to javafx.fxml;
-    exports com.mycompany.audiosign;
-}
