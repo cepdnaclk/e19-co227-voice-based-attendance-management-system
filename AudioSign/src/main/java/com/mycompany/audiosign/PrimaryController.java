08b79d8dda7432c424a901bda058ff1e56944725
spmwitharana <e19505@eng.pdn.ac.lk>
2023-09-18 22:47:00 +0530

    Third commit

--- AudioSign/src/main/java/com/mycompany/audiosign/PrimaryController.java
+++ AudioSign/src/main/java/com/mycompany/audiosign/PrimaryController.java
@@ -1,12 +1,31 @@
 package com.mycompany.audiosign;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class PrimaryController {
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    /* private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    } */
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void back(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
--- AudioSign/src/main/java/com/mycompany/audiosign/SecondaryController.java
+++ AudioSign/src/main/java/com/mycompany/audiosign/SecondaryController.java
@@ -1,12 +1,32 @@
 package com.mycompany.audiosign;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class SecondaryController {
 
     @FXML
-    private void switchToPrimary() throws IOException {
+    /*private void switchToPrimary() throws IOException {
         App.setRoot("primary");
+    } */
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void back(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
