165c85c0330bb392f07135e88706acb548814274
spmwitharana <e19505@eng.pdn.ac.lk>
2023-10-20 12:40:49 +0530

    modify

--- AudioSign/src/main/java/com/mycompany/audiosign/App.java
+++ AudioSign/src/main/java/com/mycompany/audiosign/App.java
@@ -4,22 +4,26 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
 
 /**
  * JavaFX App
  */
 public class App extends Application {
-
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("hello-view"));
+        Rectangle winSize = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds(); // This is used to get the maximum screen height and width
+
+        scene = new Scene(loadFXML("MainView"),winSize.width,winSize.height); // Initialze scene with MainView.fxml file
         stage.setScene(scene);
-        stage.show();
+        stage.setMaximized(true); // This is used to maximize the window.
+        stage.show(); // Show the UI
     }
 
     static void setRoot(String fxml) throws IOException {
@@ -27,12 +31,12 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml")); // FXMLLoader is used to load the fxml file
         return fxmlLoader.load();
     }
 
     public static void main(String[] args) {
         launch();
-    }
+    } // Launch the app
 
 }
--- AudioSign/src/main/java/com/mycompany/audiosign/DatabaseConnection.java
+++ AudioSign/src/main/java/com/mycompany/audiosign/DatabaseConnection.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    public static Connection connect() {
-        String url = "jdbc:sqlite:C:\\sqlite-voice.db"; // Replace with your SQLite database file path
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-            System.out.println("Connected to the SQLite database.");
-        } catch (SQLException e) {
-            System.err.println("Error connecting to the database: " + e.getMessage());
-        }
-        return conn;
+    // This is used to initialize database connection
+    public static Connection connect() throws SQLException {
+        String workingDir = System.getProperty("user.dir"); // Working directory path
+        String path = workingDir + "\\src\\main\\res\\database\\studentData.db"; // Absolute path for the database
+        String url = "jdbc:sqlite:" + path; // url for the database
+        //System.out.println(url);
+        String username = ""; // Username - Empty in localhost
+        String password = ""; // Password - Empty in localhost
+        Connection connection = null;
+        connection = DriverManager.getConnection(url, username, password); // Initialize the connection
+        return connection;
     }
 }
